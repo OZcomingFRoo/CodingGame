afc58553c2958835c02c3604878eed670ecaee2c
OZcomingFRoo <OZcomingFRoo@gmail.com>
2019-01-29 22:07:08 +0200

    - Completed task "temperatures" - Edited temperatures's md file

---
+++ CodingGame/Tasks/Temperatures/Temperatures.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int result = int.MaxValue;
+        int resultDistance = int.MaxValue;
+        int n = int.Parse(Console.ReadLine()); // the number of temperatures to analyse
+        string[] inputs = Console.ReadLine().Split(' ');
+        for (int i = 0; i < n; i++)
+        {
+            int t = int.Parse(inputs[i]); // a temperature expressed as an integer ranging from -273 to 5526
+            int tDistnace = DistanceFrom0(t);
+            if (tDistnace < resultDistance)
+            {
+                result = t;
+                resultDistance = tDistnace;
+
+            }
+            else if (tDistnace == resultDistance)
+            {
+                result = Math.Max(t, result);
+            }
+        }
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+        Console.WriteLine(result == int.MaxValue ? 0 : result);
+    }
+    static int DistanceFrom0(int num)
+    {
+        return num < 0 ? (-1 * num) : num;
+    }
+}
+
