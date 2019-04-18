36ec76a5dc2a9a94c4f110ee8a3230b67c85ca21
OZcomingFRoo <OZcomingFRoo@gmail.com>
2019-04-19 00:15:49 +0300

    Completed new task (not yet polished)

---
+++ CodingGame/Tasks/SoTK_EP1/SoTK_EP1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+class SoTK_EP1
+{
+    private class Location
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int minV { get; set; }
+        public int minH { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Location(int x, int y)
+        {
+            X = x;
+            Y = y;
+            minH = 0;
+            minV = 0;
+        }
+
+        public void NextStep(string direction)
+        {
+            string vertical = "UD", horizontal = "LR";
+            foreach (char dir in direction)
+            {
+                if (vertical.Contains(dir))
+                {
+                    int current = Y, maxLength = Height, startIndex = minV;
+                    BinarySearch(ref current, ref maxLength, ref startIndex, dir);
+                    Y = current; Height = maxLength; minV = startIndex;
+                }
+                else if (horizontal.Contains(dir))
+                {
+                    int current = X, maxLength = Width, startIndex = minH;
+                    BinarySearch(ref current, ref maxLength, ref startIndex, dir);
+                    X = current; Width = maxLength; minH = startIndex;
+                }
+            };
+        }
+
+        private static void BinarySearch(ref int current, ref int maxLength, ref int startIndex, char direction)
+        {
+            if ("DR".Contains(direction)) // Get middle location between current - maxLength
+            {
+                startIndex = current;
+                double distance = maxLength - startIndex;
+                int steps = (int)Math.Ceiling(distance / 2);
+                current = current + steps;
+            }
+            else // Get middle location between start/0 - current
+            {
+                maxLength = current;
+                double distance = maxLength - startIndex;
+                int steps = (int)Math.Ceiling(distance / 2);
+                current = current - steps;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", X, Y);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int width = int.Parse(inputs[0]); // width of the building.
+        int height = int.Parse(inputs[1]); // height of the building.
+        int turns = int.Parse(Console.ReadLine()); // maximum number of turns before game over.
+        inputs = Console.ReadLine().Split(' ');
+        // Starting position of batman
+        int X0 = int.Parse(inputs[0]);
+        int Y0 = int.Parse(inputs[1]);
+        Location batman = new Location(X0, Y0) { Height = height, Width = width };
+        // ================================= GAME LOOP  ================================= //
+        while (true)
+        {
+            // the direction (U, UR, R, DR, D, DL, L or UL)
+            string bombDir = Console.ReadLine();
+            batman.NextStep(bombDir);
+            // the location of the next window Batman should jump to.
+            Console.WriteLine(batman.ToString());
+        }
+    }
+
+}
+// Write an action using Console.WriteLine()
+// To debug: Console.Error.WriteLine("Debug messages...");
---
+++ CodingGame/Tasks/SoTK_EP1/SoTK_EP1.md
@@ -0,0 +1,13 @@
+﻿
+### Shadows of the Knight - Episode 21
+
+**Task Description :**
+
+The goal of this puzzle is to guess the coordinate of a bomb (line and column of a 2 dimensional array).
+
+You will have to make a guess at each step of the puzzle and adjust it from given feedbacks.
+
+Of course, you have a limited number of guess.
+
+
+[Link to task, copy and paste the code](https://www.codingame.com/ide/puzzle/shadows-of-the-knight-episode-1)
